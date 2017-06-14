fc89464f0ecbfdb005f45f57a60f48b19b3ca292
kgajjar <kgajjar@salesforce.com>
2017-06-14 13:48:20 -0400

    v2

--- test/features/step_definitions/parallelStepDefs.js
+++ test/features/step_definitions/parallelStepDefs.js
@@ -1,36 +1,37 @@
 'use strict';
 var htmlReporter = require('cucumber-html-reporter');
+var {defineSupportCode} = require('cucumber');
 
-module.exports = function parallelStepDefs() {
-    this.Before(function(scenario, callback) {
+defineSupportCode(function({Before, registerHandler, Given, Then, When}) {
+    Before(function(scenario, callback) {
         this.scenario = scenario;
         callback();
     });
 
-    this.Given(/^Fred has multiple (?:features|scenarios) written in cucumber$/, function (callback) {
-        this.scenario.attach(new Buffer('').toString('base64'), 'image/png');
+    Given(/^Fred has multiple (?:features|scenarios) written in cucumber$/, function (callback) {
+        this.attach(new Buffer('').toString('base64'), 'image/png');
         callback();
     });
 
-    this.When(/^he runs the features in parallel with "([^"]*)" using cucumber\-parallel module$/, function (testData, callback) {
-        this.scenario.attach(testData);
+    When(/^he runs the features in parallel with "([^"]*)" using cucumber\-parallel module$/, function (testData, callback) {
+        this.attach(testData);
         callback();
     });
 
-    this.Then(/^all the (?:features|scenarios) should run in parallel$/, function (callback) {
+    Then(/^all the (?:features|scenarios) should run in parallel$/, function (callback) {
         callback();
     });
 
-    this.When(/^Fred has scenario outline with the "([^"]*)"$/, function (id, callback) {
+    When(/^Fred has scenario outline with the "([^"]*)"$/, function (id, callback) {
         callback();
     });
 
-    this.Given(/^Fred has a step with below data table$/, function (table, callback) {
+    Given(/^Fred has a step with below data table$/, function (table, callback) {
         // Write code here that turns the phrase above into concrete actions
         callback();
     });
 
-    this.registerHandler('AfterFeatures', function(event, callback) {
+    registerHandler('AfterFeatures', function(features, callback) {
         htmlReporter.generate({
             theme: 'bootstrap',
             jsonFile: 'test/report/cucumber_report.json',
@@ -40,4 +41,4 @@ module.exports = function parallelStepDefs() {
         callback();
     });
 
-};
+});
